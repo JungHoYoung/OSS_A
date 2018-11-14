d9b9f39f8b785331c722c364e4078c663740f30d
JungHoYoung <38202517+JungHoYoung@users.noreply.github.com>
2018-11-14 16:10:19 +0900

    Update Main.java
    생성자 설정

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
 
 public class Main {
-
+    private int[][] mines;
+    private char[][] Board;
+    private int row, column;
+    Random random = new Random();
+    Scanner sc = new Scanner(System.in);
+  
+    public Main(){
+      mine = new int[10][10];
+      Board = new char[10][10];
+      initializeMines();          //초기화된 판
+      placeMines();               //지뢰를 무작위 위치로 채움
+      NominesOfSurroundingNeighbours(); //주변에 지뢰가 있는지 숫자로 표현해줌
+      startBoard();                     //게임시작
 }
